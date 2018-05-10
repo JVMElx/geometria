f2b807cd4e003da8bb56f64fddb8664eec0ecf1e
JAVIER <jvmasprella@gmail.com>
2018-05-10 14:49:29 +0200

    versión comentada

--- src/Triangulo_JVM.java
+++ src/Triangulo_JVM.java
@@ -1,9 +1,20 @@
-
+/**
+ * 
+ * @author JAVIER VICENTE MACIÁ
+ * @version 1.1
+ *
+ */
 public class Triangulo_JVM extends FiguraGeometrica_JVM {
 	private double lado1;
 	private double lado2;
 	private double lado3;
-	
+/**
+ * 	
+ * @param tipoFigura
+ * @param lado1
+ * @param lado2
+ * @param lado3
+ */
 	public Triangulo_JVM(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
 		lado1 = lado1;
@@ -11,11 +22,18 @@ public class Triangulo_JVM extends FiguraGeometrica_JVM {
 		lado3 = lado3;
 	}
 
+	
+	/**
+	 * Devuelve la suma de todos los lados del triangulo.
+	 */
 	@Override
 	public double perimetro() {
 		return lado1 + lado2 + lado3;
 	}
 
+	/**
+	 * calcula el area del triangulo
+	 */
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
