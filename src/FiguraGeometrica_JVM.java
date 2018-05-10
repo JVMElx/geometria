17cde7ac607b297535beac04a58bdd759ae511bd
JAVIER <jvmasprella@gmail.com>
2018-05-10 13:40:58 +0200

    versión refactorizada

--- src/FiguraGeometrica_JVM.java
+++ src/FiguraGeometrica_JVM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_JVM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_JVM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
